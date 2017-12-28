aa8768b4b7f75d1d96caa6f7110bc9b995c246af
Andrew <andrew.ramell@gmail.com>
2017-12-28 16:52:58 -0500

    test commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 const app = new App()
+
+document.addEventListener('DOMContentLoaded', function(){
+
+document.getElementById("#new-note-body").addEventListener('click', console.log("test"))
+console.log("test")
+})
